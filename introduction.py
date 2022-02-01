042c4cb981699c1d56fc062a41b40d7b1d3eb496
mmalikhidayatulloh <m4likhidayatulloh@gmail.com>
2022-02-01 20:11:45 +0700

    sampe tanda miring

---
+++ introduction.py
@@ -0,0 +1,41 @@
+print('bismillah')
+print("bisa")
+# untuk string bisa pake petik satu maupun petik dua,untuk angka tidak perlu pake tanda petik
+print(1+2*3-4/5)
+print()
+# jika petik satu dan petik 2 dalam satu print
+print('"sinau" sampe mati')
+# string bisa ditambah (,) bisa (+)
+print('kamu'+'siapa')
+
+
+# Variabel
+saya = "umursaya"
+umur = input("umur:")
+print(saya + ' ' + umur + " tahun")
+
+
+# \n -> untuk baris baru
+print("siapa\nAnda?")
+# \t -> tab
+print("saya\ttidak peduli")
+# tanda petik
+print("\'oke\' kalau begitu")
+
+split = """contoh
+split
+atau
+newline
+atau
+baris
+baru"""
+print(split)
+
+batalsplit ="""contoh \
+batalsplit \
+atau \
+newline \
+atau \
+baris \
+baru"""
+print(batalsplit)
